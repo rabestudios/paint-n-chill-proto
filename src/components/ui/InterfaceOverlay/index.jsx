7b4773d4289e90d410a0383fb8779389b38b54f6
Chris Rabe <chris.rabe14@gmail.com>
2020-12-05 21:56:58 +1100

    added UI icons

--- src/components/ui/InterfaceOverlay/index.jsx
+++ src/components/ui/InterfaceOverlay/index.jsx
@@ -1,12 +1,23 @@
 import styled from "styled-components";
+import InterfaceIcon from "components/ui/InterfaceIcon";
+import { ExitToApp, Group } from "@material-ui/icons";
 
 const OverlayContainer = styled.div`
   position: absolute;
+  display: flex;
+  flex-direction: column;
   z-index: 2;
 `;
 
 const InterfaceOverlay = () => (
-  <OverlayContainer>Inteface Overlay</OverlayContainer>
+  <OverlayContainer>
+    <InterfaceIcon label="Leave Room">
+      <ExitToApp fontSize="large" />
+    </InterfaceIcon>
+    <InterfaceIcon label="View Players">
+      <Group fontSize="large" />
+    </InterfaceIcon>
+  </OverlayContainer>
 );
 
 export default InterfaceOverlay;
